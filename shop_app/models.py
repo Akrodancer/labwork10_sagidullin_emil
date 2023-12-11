0271e9605c9783a49517edb864555ed1be1521d4
SagidullinEmil <se12373@auca.kg>
2023-12-11 20:21:50 +0600

    Добавление поля remainder и изменение поля price в моделях. Создание и применение миграций

--- shop_app/models.py
+++ shop_app/models.py
@@ -16,9 +16,10 @@ class Product(models.Model):
     name = models.CharField(max_length=50, verbose_name="Наименование", null=False, blank=False)
     description = models.TextField(max_length=1000, null=True, blank=True, verbose_name="Описание")
     date = models.DateField(auto_now_add=True, verbose_name='Время добавления')
-    price = models.DecimalField(max_digits=50, decimal_places=0, null=False, blank=False, verbose_name="Цена")
+    price = models.DecimalField(max_digits=7, decimal_places=2, null=False, blank=False, verbose_name="Цена")
     product_category = models.ForeignKey('shop_app.Category',  null=False, blank=False, on_delete=models.RESTRICT, verbose_name='Категория', related_name='categories', )
     image = models.TextField(max_length=1000, null=False, blank=False,  verbose_name='Изображение')
+    remainder = models.DecimalField(max_digits=20, decimal_places=0, default=1, null=False, blank=False, verbose_name="Остаток")
 
     def __str__(self):
         return f'{self.pk}. {self.name}. {self.description}. {self.date}. {self.price}'
