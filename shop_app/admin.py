5ace7ee1cab9b24b486b69b366ad54b3da749eaf
SagidullinEmil <se12373@auca.kg>
2023-12-12 11:26:57 +0600

    Добавление форм продуктов и категорийб изменения в админке

--- shop_app/admin.py
+++ shop_app/admin.py
@@ -1,19 +1,18 @@
 from django.contrib import admin
 from shop_app.models import Category, Product
-
 # Register your models here.
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['id', 'name', 'description']
-    list_display_links = ['id']
+    list_display_links = ['name']
     list_filter = ['name']
     search_fields = ['name']
-    fields = ['id', 'name', 'description']
+    fields = ['name', 'description']
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['id', 'name', 'description', 'date', 'price', 'product_category']
+    list_display = ['id', 'name', 'description', 'date', 'price', 'product_category', 'remainder']
     list_display_links = ['id', 'name']
     list_filter = ['date']
     search_fields = ['date', 'product_category']
-    fields = ['id', 'name', 'description', 'date', 'price', 'product_category']
+    fields = [ 'name', 'description', 'price', 'product_category']
---
+++ shop_app/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from django.forms import widgets
+from shop_app.models import Category
+
+
+class CategoryForm(forms.Form):
+    name = forms.CharField(max_length=50, label="Наименование", required=True)
+    description = forms.CharField(max_length=1000, required=False, label="Описание", widget=widgets.Textarea)
+
+
+class ProductForm(forms.Form):
+    name = forms.CharField(max_length=50, label="Наименование", required=True)
+    description = forms.CharField(max_length=1000, required=False, label="Описание", widget=widgets.Textarea)
+    date = forms.CharField(auto_now_add=True, label='Время добавления')
+    price = forms.DecimalField(max_digits=7, decimal_places=2, required=True, label="Цена")
+    product_category = forms.ModelChoiceField(Category.object.all())
+    image = forms.CharField(max_length=1000, required=True, label='Изображение')
+    remainder = forms.DecimalField(max_digits=20, decimal_places=0, default=1, required=True, label="Остаток")
